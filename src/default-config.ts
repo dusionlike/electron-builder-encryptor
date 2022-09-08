6f01de937e6597cd1403ef9a9fa467530d0a71ff
zhangzhixiong <751069244@qq.com>
2022-09-08 14:55:17 +0800

    feat: merge config using `defu`

--- src/default-config.ts
+++ src/default-config.ts
@@ -3,7 +3,8 @@ import type { UserConfigExport } from './config'
 export function mergeDefaultConfig(
   arg: UserConfigExport
 ): Required<UserConfigExport> {
-  return Object.assign(
+  const defu = createDefu()
+  return defu(
     {
       key: 'ft*xx9527',
       protocol: 'myclient',
@@ -25,3 +26,67 @@ export function mergeDefaultConfig(
     arg
   )
 }
+
+function isObject(val: any) {
+  return val !== null && typeof val === 'object'
+}
+
+// from https://github.com/unjs/defu
+// Base function to apply defaults
+function _defu<T>(
+  baseObj: T,
+  defaults: any,
+  namespace = '.',
+  merger?: Merger
+): T {
+  if (!isObject(defaults)) {
+    return _defu(baseObj, {}, namespace, merger)
+  }
+
+  const obj = Object.assign({}, defaults)
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key in baseObj) {
+    if (key === '__proto__' || key === 'constructor') {
+      continue
+    }
+
+    const val = baseObj[key]
+
+    if (val === null || val === undefined) {
+      continue
+    }
+
+    if (merger && merger(obj, key, val, namespace)) {
+      continue
+    }
+
+    if (Array.isArray(val) && Array.isArray(obj[key])) {
+      obj[key] = val.concat(obj[key])
+    } else if (isObject(val) && isObject(obj[key])) {
+      obj[key] = _defu(
+        val,
+        obj[key],
+        (namespace ? `${namespace}.` : '') + key.toString(),
+        merger
+      )
+    } else {
+      obj[key] = val
+    }
+  }
+
+  return obj
+}
+
+// Create defu wrapper with optional merger and multi arg support
+export function createDefu(): any {
+  return (...args: any[]) => args.reduce((p, c) => _defu(p, c, ''), {} as any)
+}
+
+export type Input = Record<string | number | symbol, any>
+export type Merger = <T extends Input, K extends keyof T>(
+  obj: T,
+  key: keyof T,
+  value: T[K],
+  namespace: string
+) => any
